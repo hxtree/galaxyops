7fa4dce2965213c1856fb34eda05f762137ce999
hxtree <matthewheroux@gmail.com>
2022-05-17 23:44:17 -0500

    Fleshing out DTM

--- apis/damage-type-modifiers/src/DamageTypes.ts
+++ apis/damage-type-modifiers/src/DamageTypes.ts
@@ -1,4 +1,4 @@
-export enum DamageType {
+export enum Type {
     Electric, // this is the damage type modifier for lightning. 
     Air, // this is the damage type modifier for wind.
     Earth, // this is the damage type modifier for earth.
@@ -11,72 +11,101 @@ export enum DamageType {
     Light // this is the damage type modifier for light.
 }
 
-// asborbtion necessates group processing of damage type to area of impact
-// stackable, and entity may be have multiple DamageEffects for a single type.
-
-export enum DamageEffect {
+// absorbtion necessates group processing of damage type to area of impact
+export enum Effect {
     Resistance, // decreases damage taken by the damage type.
     Weakness, // increases damage taken by the damage type.
     Nullify, // completely nullifies damage taken by the damage type.
     Immunity, // completely blocks damage taken by the damage type.
     Absorption, // reduces damage type area of effect to nearby targets.
-    Healing, // completely heals damage taken by the damage type.
+    Healing, // reverses damage taken into healing by the damage type.
 }
 
-
-
-export enum DamageOperator {
-    '*',
-    '-',
-    '+',
-    '/',
+export enum Operator {
+    '*', // multiply
+    '-', // subtract
+    '+', // add
+    '/' // divide
 }
 
-export type DamageEffectModifier = Number;
-export type DamageAmount = Number;
+export type Modifier = Number;
+
+export type DTM = [Type, Effect, Operator, Modifier];
+export type DTMs = Array<DTM>;
+
+
+export type Amount = Number;
+export type Damage = [Amount, Type];
+export type Damages = Array<Damage>;
+
 export type DamageTotal = Number;
 
-
+// Can damages only effect HP or can they effect MP?
 
 export class DamageTypeModifier {
 
-    private ExampleCharacterEffect = [
-        ["Electric", "Resistance", "-", "200"],
-        ["Electric", "Resistance", "/", "2"],
-        ["Electric", "Absorption", "/", "4"],
-        ["Air", "Weakness", "+", "100"],
-        ["Earth", "Nullify", "-", "0"],
-        ["Fire", "Immunity", "-", "0"],
-        ["Water", "Absorption", "-", "0"],
-        ["Nature", "Healing", "-", "0"]
-    ];
+    public proccess(damage: Damage, dtms: DTMs): DamageTotal {
+        const amount = damage[0];
+        let total: DamageTotal = 0;
+        
 
-    // todo need to determine where roll occurs
-    private ExampleAction = [
-        ["200", "Fire"],
-        ["1000", "Physical"],
-    ];
-    private ExampleActionWithRoll = [
-        ["2d2+5", "Fire"],
-        ["51", "Physical"],
-        ["40d6", "Physical"],
-    ];
+        // stackable, and entity may be have multiple DamageEffects for a single type.
+        dtms.forEach(function (dtm) {
+            if(dtm[0] == damage[1]){
+                // todo
+
+            }
+        });
+        return total;
+    }
+
+    // perhaps move to API
+    public batchProcess(damages: Damages, dtms: DTMs) : DamageTotal {
+        let total: DamageTotal = 0;
+        for (var damage of damages) {
+            total = this.proccess(damage, dtms);
+        }
+        return total;
+    }
+
+    // resolve rolls
+    // adjust rolls for impacted parties using DTM
 
     
     // Actor Deals -> calculate -> Subject Takes
     // Action
     // 100, Fire, Resistance, *, 1.5
 
-    public calculateDamage(damageType: DamageType, damageEffect: DamageEffect, damageOperator: DamageOperator, damageEffectModifier: DamageEffectModifier): DamageTotal {
-        return 0;
-    }
-
-    // perhaps move to API
-    public batchProcessor(): DamageTotal {
-        return 0;
-    }
-
+    // what about status effects caused by damages :?
 }
+
+
+// should another var be status, MP, HP, SP?
+const ExampleCharacterDamageTypeEffect = [
+    ["Physical", "Resistance", "%", "5"], // effect of armor
+    ["Physical", "Resistance", "-", "50"], // effect of armor
+    ["Electric", "Resistance", "-", "200"], // natural ability
+    ["Electric", "Resistance", "/", "2"], // natural ability
+    ["Electric", "Absorption", "/", "4"], // natural ability
+    ["Air", "Weakness", "+", "100"], // natural ability
+    ["Earth", "Nullify", "-", "0"], // effect of magic ring
+    ["Fire", "Immunity", "-", "0"], // effect of magic necklace
+    ["Water", "Absorption", "-", "0"], // effect of magic ring
+    ["Nature", "Healing", "+", "20"] // effect of tunic 
+];
+
+// todo need to determine where roll occurs
+const ExampleAction = [
+    ["200", "Fire"],
+    ["1000", "Physical"],
+];
+
+const ExampleActionWithRoll = [
+    ["2d2+5", "Fire"],
+    ["51", "Physical"],
+    ["40d6", "Physical"],
+];
+
 
 
 // DTM can be 
