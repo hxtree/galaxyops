15e27c3fff592868f07d82e317846396febf86e4
hxtree <matthew.heroux@gmail.com>
2022-10-26 03:48:10 +0000

    wip: saving

---
+++ packages/api-gateway/README.md
@@ -0,0 +1,7 @@
+# API Gateway
+
+Getting to the point where coordination between services is desirable:
+
+https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/walkthrough-crossstackref.html
+
+https://aws.amazon.com/blogs/compute/architecting-multiple-microservices-behind-a-single-domain-with-amazon-api-gateway/
--- packages/luck-by-dice-monitor/stacks/LuckByDiceClientStack.ts
+++ packages/luck-by-dice-monitor/stacks/LuckByDiceClientStack.ts
@@ -1,8 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import {Bucket, BucketAccessControl} from "aws-cdk-lib/aws-s3";
-import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
-import {Distribution, OriginAccessIdentity, ResponseHeadersPolicy} from "aws-cdk-lib/aws-cloudfront";
-import {S3Origin} from "aws-cdk-lib/aws-cloudfront-origins";
+import {Bucket, BucketAccessControl} from 'aws-cdk-lib/aws-s3';
+import {BucketDeployment, Source} from 'aws-cdk-lib/aws-s3-deployment';
+import {
+  Distribution,
+  OriginAccessIdentity,
+  ResponseHeadersPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
+import {S3Origin} from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as route53 from 'aws-cdk-lib/aws-route53';
 
 export class LuckByDiceClientStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -11,9 +17,12 @@ export class LuckByDiceClientStack extends cdk.Stack {
     // s3 bucket
     const bucket = new Bucket(this, 'Bucket', {
       accessControl: BucketAccessControl.PRIVATE,
-    })
+    });
 
-    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+    const originAccessIdentity = new OriginAccessIdentity(
+      this,
+      'OriginAccessIdentity',
+    );
     bucket.grantRead(originAccessIdentity);
 
     // cloudfront distribution
@@ -23,34 +32,39 @@ export class LuckByDiceClientStack extends cdk.Stack {
       defaultBehavior: {
         origin: new S3Origin(bucket, {originAccessIdentity}),
         // todo lock down CORS later
-        responseHeadersPolicy: ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS_WITH_PREFLIGHT
+        responseHeadersPolicy:
+          ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS_WITH_PREFLIGHT,
       },
     });
 
-    import * as s3 from '@aws-cdk/aws-s3';
+    new cdk.CfnOutput(this, 'Domain Name', {
+      value: distribution.domainName,
+    });
 
-const recordName = 'monitor';
-const domainName = 'ouxsoft.com';
+    // const recordName = 'monitor';
+    // const domainName = 'ouxsoft.com';
 
-const bucketWebsite = new s3.Bucket(this, 'BucketWebsite', {
-  bucketName: [recordName, domainName].join('.'), // www.example.com
-  publicReadAccess: true,
-  websiteIndexDocument: 'index.html',
-});
+    // const bucketWebsite = new s3.Bucket(this, 'BucketWebsite', {
+    //   bucketName: [recordName, domainName].join('.'), // www.example.com
+    //   publicReadAccess: true,
+    //   websiteIndexDocument: 'index.html',
+    // });
 
-const zone = route53.HostedZone.fromLookup(this, 'Zone', {domainName}); // example.com
+    // const zone = route53.HostedZone.fromLookup(this, 'Zone', {domainName}); // example.com
 
-new route53.ARecord(this, 'AliasRecord', {
-  zone,
-  recordName, // www
-  target: route53.RecordTarget.fromAlias(new targets.BucketWebsiteTarget(bucketWebsite)),
-});
+    // new route53.ARecord(this, 'AliasRecord', {
+    //   zone,
+    //   recordName, // www
+    //   target: route53.RecordTarget.fromAlias(
+    //     new targets.BucketWebsiteTarget(bucketWebsite),
+    //   ),
+    // });
 
-    // bucket resource
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const awsBucketResource = new BucketDeployment(this, 'BucketDeployment', {
-      destinationBucket: bucket,
-      sources: [Source.asset('dist')]
-    });   
+    // // bucket resource
+    // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const awsBucketResource = new BucketDeployment(this, 'BucketDeployment', {
+    //   destinationBucket: bucket,
+    //   sources: [Source.asset('dist')],
+    // });
   }
 }
