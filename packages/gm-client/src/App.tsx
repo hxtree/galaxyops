6e896bf6ee6fe5badc6a75bed5956e73a50c758c
hxtree <matthew.heroux@gmail.com>
2022-11-19 05:30:17 +0000

    feat: add GM Client links

---
+++ packages/gm-client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Counter} from './features/counter/Counter';
+import {MainNavigation} from './features/main-navigation/MainNavigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Game Master Client
+        <MainNavigation />
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
