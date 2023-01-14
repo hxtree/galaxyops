2082337da7476d9cc9db6c35c15af90c7f4a2c82
hxtree <matthew.heroux@gmail.com>
2023-01-14 18:19:01 +0000

    feat: add code coverage gutter and mock library
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

---
+++ libraries/mock-factory/src/__tests__/mock-factory.test.ts
@@ -0,0 +1,59 @@
+import { MockFactory } from '../mock-factory';
+import { ExampleClass } from './example-class';
+
+describe('MockFactory', () => {
+  describe('create', () => {
+    let mock: ExampleClass;
+
+    beforeAll(() => {
+      mock = MockFactory.create<ExampleClass>(ExampleClass);
+    });
+
+    it('should create a mock object', () => {
+      expect(typeof mock).toBe('object');
+    });
+
+    it('should create boolean based on decorators', () => {
+      expect(typeof mock.boolean).toBe('boolean');
+    });
+
+    it('should create number based on decorators', () => {
+      expect(typeof mock.number).toBe('number');
+    });
+
+    it('should create string based on decorators', () => {
+      expect(typeof mock.string).toBe('string');
+    });
+
+    it('should create uuid based on decorators', () => {
+      expect(typeof mock.uuid).toBe('string');
+    });
+
+    it('should create currency based on decorators', () => {
+      expect(typeof mock.currency).toBe('string');
+    });
+
+    it('should create email based on decorators', () => {
+      expect(typeof mock.email).toContain('string');
+      expect(mock.email).toContain('@');
+    });
+
+    it('should create site based on decorators', () => {
+      expect(typeof mock.site).toContain('string');
+      expect(mock.site).toContain('.');
+    });
+
+    it('should create mock object using seeded data', () => {
+      const partial = {
+        number: 2131,
+        string: 'Test data',
+        boolean: true,
+      };
+      mock = MockFactory.create<ExampleClass>(ExampleClass, partial);
+
+      expect(mock.number).toBe(partial.number);
+      expect(mock.string).toBe(partial.string);
+      expect(mock.boolean).toBe(partial.boolean);
+    });
+  });
+});
