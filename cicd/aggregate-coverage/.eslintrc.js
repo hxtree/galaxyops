9638101b50ed901548b9454cab68012f0ae83a5b
Matthew Heroux <matthewheroux@gmail.com>
2023-01-25 22:15:40 -0600

    feat: aggregate coverage (#89)
    Create scafolding for aggregate coverage repo.
    
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

---
+++ cicd/aggregate-coverage/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['@org-platform/eslint-config/profile/base'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+};
---
+++ cicd/aggregate-coverage/src/index.ts
@@ -0,0 +1,21 @@
+console.log('WIP');
+// import { createRequire } from 'module';
+// import istanbulCoverage from 'istanbul-lib-coverage';
+// import istanbulReport from 'istanbul-lib-report';
+// import istanbulReports from 'istanbul-reports';
+
+// // // TODO find all coverage report paths
+// // const destination = '../coverage/coverage-final.json';
+
+// // // combine all coverage reports
+// // const map = istanbulCoverage.createCoverageMap();
+
+// // Object.keys(coverage).forEach(filename =>
+// //   map.addFileCoverage(mapFileCoverage(coverage[filename])),
+// // );
+
+// // const context = istanbulReport.createContext({ coverageMap: map });
+
+// // ['lcov'].forEach(reporter =>
+// //   istanbulReports.create(reporter, {}).execute(context),
+// // );
