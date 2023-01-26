25dc5e5a8605617d1998f229103cec9945bf8399
hxtree <matthew.heroux@gmail.com>
2023-01-26 13:40:29 +0000

    feat: get all coverage reports
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- cicd/aggregate-coverage/src/index.ts
+++ cicd/aggregate-coverage/src/index.ts
@@ -1,14 +1,26 @@
-console.log('WIP');
+import fs from 'fs';
 // import { createRequire } from 'module';
 // import istanbulCoverage from 'istanbul-lib-coverage';
 // import istanbulReport from 'istanbul-lib-report';
 // import istanbulReports from 'istanbul-reports';
+import { getProjects } from './getProjects';
 
-// // // TODO find all coverage report paths
-// // const destination = '../coverage/coverage-final.json';
+// const destination = '../coverage/coverage-final.json';
+const projects = getProjects();
+const coverageFiles: String[] = [];
 
-// // // combine all coverage reports
-// // const map = istanbulCoverage.createCoverageMap();
+projects.forEach((project: any) => {
+  const projectCoverageFile = `${project.projectFolder}/coverage/lcov.info`;
+  if (fs.existsSync(projectCoverageFile)) {
+    coverageFiles.push(projectCoverageFile);
+  }
+});
+
+console.log(coverageFiles);
+
+// TODO combine all coverage reports
+
+// const map = istanbulCoverage.createCoverageMap();
 
 // // Object.keys(coverage).forEach(filename =>
 // //   map.addFileCoverage(mapFileCoverage(coverage[filename])),
