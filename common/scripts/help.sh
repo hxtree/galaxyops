e30cf5cb394300b96aaffd65125f69f16c85715e
hxtree <matthew.heroux@gmail.com>
2023-02-07 12:28:35 +0000

    feat: update help
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- common/scripts/help.sh
+++ common/scripts/help.sh
@@ -13,16 +13,17 @@ rush add -p @a/b           add a new package to app
  --make-consistent         make package version consistant across monorepo
  --dev                     install as dev dependency
 rush upgrade-interactive   interactively update packages
+ --make-consistent         make package version consistant across monorepo
 
 rushx [command]      run a command relative to the current working directory.
-rushx bootstrap      bootstrap deploy for the first time
+rushx cdk:bootstrap  bootstrap deploy for the first time
 rushx build          build projects
-rushx test           test a package within directory
 rushx lint           eslint static code analysis
+rushx test           test a package within directory
 rushx docs           generate documents from code
-rushx synth          show synthesized CloudFormation template
-rushx diff           compare deployed stack with current state
-rushx deploy         deploy apps using AWS CDK
+rushx cdk:synth      show synthesized CloudFormation template
+rushx cdk:diff       compare deployed stack with current state
+rushx cdk:deploy     deploy apps using AWS CDK
 
 npm run watch        watch for changes and compile
 cdk deploy --hotswap speed up dev deployment
