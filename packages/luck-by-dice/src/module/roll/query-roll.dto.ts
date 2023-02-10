6176cc3f91ae107ced2d2cc069671f07b3fcbfe5
hxtree <matthew.heroux@gmail.com>
2023-02-10 06:08:37 +0000

    feat: mv dice endpoints
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- packages/luck-by-dice/src/module/roll/query-roll.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class QueryRollDto {
-  @ApiProperty()
-  notation: string;
-
-  @ApiProperty()
-  luck: number;
-}
--- packages/luck-by-dice/src/module/roll/response-roll.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class ResponseRollDto {
-  @ApiProperty({ enum: ['Success', 'Error', 'Fail'] })
-  message: string;
-
-  @ApiProperty()
-  data: string[];
-}
