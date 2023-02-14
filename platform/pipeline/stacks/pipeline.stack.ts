a3572e02a2ffc34184039b4e0dc0a80e89e60f0e
hxtree <matthew.heroux@gmail.com>
2023-02-14 04:11:41 +0000

    feat: pub clients
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- platform/pipeline/stacks/pipeline.stack.ts
+++ platform/pipeline/stacks/pipeline.stack.ts
@@ -7,19 +7,17 @@ import {
 } from 'aws-cdk-lib/pipelines';
 
 export class MyPipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    const owner = 'hxtree';
-    const repo = 'cats-cradle';
-    const branch = 'main';
-
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub(`${owner}/${repo}`, branch),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
-      }),
-    });
-  }
+  // constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  //   super(scope, id, props);
+  //   const owner = 'hxtree';
+  //   const repo = 'cats-cradle';
+  //   const branch = 'main';
+  //   const pipeline = new CodePipeline(this, 'Pipeline', {
+  //     pipelineName: 'MyPipeline',
+  //     synth: new ShellStep('Synth', {
+  //       input: CodePipelineSource.gitHub(`${owner}/${repo}`, branch),
+  //       commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+  //     }),
+  //   });
+  // }
 }
