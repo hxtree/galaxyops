7c94fa0b04f7e436e8b73525f7ecec90991ce5b9
hxtree <matthew.heroux@gmail.com>
2023-02-18 16:33:55 +0000

    feat: add e2e delete tests to character sheet
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- packages/character-sheet/src/module/character-sheet/character-sheet.service.ts
+++ packages/character-sheet/src/module/character-sheet/character-sheet.service.ts
@@ -4,8 +4,4 @@ import { CharacterSheetRepository } from './character-sheet.repository';
 @Injectable()
 export class CharacterSheetService {
   constructor(private _characterSheetRepository: CharacterSheetRepository) {}
-
-  async findOne(id: string): Promise<any> {
-    return this._characterSheetRepository.findOne({ id });
-  }
 }
