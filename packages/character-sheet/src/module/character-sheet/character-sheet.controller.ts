9c4e87109fcd6260346d695c57864108be727a99
Matthew Heroux <matthewheroux@gmail.com>
2023-02-18 10:36:57 -0600

    feat: add e2e delete tests to character sheet (#176)
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- packages/character-sheet/src/module/character-sheet/character-sheet.controller.ts
+++ packages/character-sheet/src/module/character-sheet/character-sheet.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import {
+  NotFoundException,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { CharacterSheetRepository } from './character-sheet.repository';
 import { CharacterSheetService } from './character-sheet.service';
 
 @Controller('/character-sheets')
 export class CharacterSheetController {
-  constructor(private _characterSheetService: CharacterSheetService) {}
+  constructor(
+    private _characterSheetService: CharacterSheetService,
+    private _characterSheetRepository: CharacterSheetRepository,
+  ) {}
 
   @Get(':id')
   async findOne(@Param('id', new ParseUUIDPipe()) id: string): Promise<any> {
-    return this._characterSheetService.findOne(id);
+    const result = await this._characterSheetRepository.findOne({
+      id,
+    });
+
+    if (!result) {
+      throw new NotFoundException();
+    }
+    return result;
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', new ParseUUIDPipe()) id: string): Promise<any> {
+    return this._characterSheetRepository.delete({
+      id,
+    });
   }
 }
--- packages/character-sheet/src/module/character-sheet/character-sheet.e2e-spec.ts
+++ packages/character-sheet/src/module/character-sheet/character-sheet.e2e-spec.ts
@@ -10,6 +10,7 @@ import { FakerFactory } from '@cats-cradle/faker-factory';
 import { CharacterSheetService } from './character-sheet.service';
 import { CharacterSheetSchema, CharacterSheet } from './character-sheet.schema';
 import { CharacterSheetRepository } from './character-sheet.repository';
+import { CharacterSheetController } from './character-sheet.controller';
 
 describe('/character-sheets', () => {
   let app: INestApplication;
@@ -25,6 +26,7 @@ describe('/character-sheets', () => {
         ]),
       ],
       providers: [CharacterSheetRepository, CharacterSheetService],
+      controllers: [CharacterSheetController],
     }).compile();
 
     app = moduleRef.createNestApplication();
@@ -45,9 +47,10 @@ describe('/character-sheets', () => {
 
   describe('GET /character-sheets/:id', () => {
     it('should not find results that do not exists', async () => {
-      await supertest(app.getHttpServer())
+      const response = await supertest(app.getHttpServer())
         .get('/character-sheets/1f251943-174c-4ee0-87e4-0a52ed2eebbc')
         .expect(404);
+      expect(response.body).toEqual({ message: 'Not Found', statusCode: 404 });
     });
 
     it('should find result if exists', async () => {
@@ -56,9 +59,29 @@ describe('/character-sheets', () => {
       );
       await characterSheetRepository.create(characterSheet);
 
-      await supertest(app.getHttpServer())
+      const result = await supertest(app.getHttpServer())
         .get(`/character-sheets/${characterSheet.id}`)
-        .expect(404);
+        .expect(200);
+      expect(result.body).toEqual(expect.objectContaining(characterSheet));
+    });
+  });
+
+  describe('DELETE /character-sheets/:id', () => {
+    it('should delete result if exists', async () => {
+      const characterSheet = await FakerFactory.create<CharacterSheet>(
+        CharacterSheet,
+      );
+      await characterSheetRepository.create(characterSheet);
+
+      const result = await supertest(app.getHttpServer())
+        .delete(`/character-sheets/${characterSheet.id}`)
+        .expect(200);
+      expect(result.body).toEqual(
+        expect.objectContaining({
+          deleted: true,
+          deletedCount: 1,
+        }),
+      );
     });
   });
 });
