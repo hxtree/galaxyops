8ae83ae73d1cb9f343a48b910656fa5fa2489389
hxtree <matthew.heroux@gmail.com>
2023-02-21 14:45:44 +0000

    feat: flesh in recordkeeping
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- packages/email-service/src/module/template/template.service.ts
+++ packages/email-service/src/module/template/template.service.ts
@@ -6,17 +6,37 @@ import { FormatType } from './format.type';
 
 @Injectable()
 export class TemplateService {
+  async convertToFormat(
+    templateId: string,
+    data: any,
+    format: FormatType,
+  ): Promise<any> {
+    const { html, text, template } = await this.convert(templateId, data);
+
+    switch (format) {
+      case FormatType.HTML:
+        return await Promise.resolve({ data: html });
+      case FormatType.TEXT:
+        return await Promise.resolve({ data: text });
+      case FormatType.TEMPLATE:
+        return await Promise.resolve({ data: template });
+      default:
+        return await Promise.resolve({ html, text });
+    }
+  }
+
   async convert(template: string, dto: any): Promise<any> {
-    const html = await this.parse(template, dto);
+    const data = instanceToPlain(dto)[0];
+    const html = await this.parse(template, data);
     const text = await this.toText(html);
 
     return { html, text, template } as const;
   }
 
-  async parse(template: string, params: any): Promise<string> {
-    const varaibles = instanceToPlain(params);
+  async parse(template: string, data: any): Promise<string> {
     const templateCompiled = Handlebars.compile(template);
-    return templateCompiled(varaibles);
+    const output = templateCompiled(data);
+    return output;
   }
 
   async toText(html: string): Promise<string> {
