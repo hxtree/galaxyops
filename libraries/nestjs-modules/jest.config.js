7817fb3b2ee3ada46f5c8e27cc12c30c0a16e060
hxtree <matthew.heroux@gmail.com>
2023-03-16 03:36:22 +0000

    feat: unify env
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- libraries/nestjs-modules/jest.config.js
+++ libraries/nestjs-modules/jest.config.js
@@ -1,17 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
-  testRegex: '.*\\.test\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
+  preset: 'ts-jest',
   testEnvironment: 'node',
-
-  moduleNameMapper: {
-    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
-    uuid: require.resolve('uuid'),
-  },
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**'],
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  coverageDirectory: '<rootDir>/coverage',
 };
