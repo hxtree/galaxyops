4666560175aaa264ae35678d8cf12ff1db75453c
Matthew Heroux <matthewheroux@gmail.com>
2023-03-16 19:54:59 -0500

    fix: bugfix shebang cov (#246)
    ---------
    
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- platform/rush-coverage/src/index.ts
+++ platform/rush-coverage/src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { getCoverageReports } from './get-coverage-reports';
 import { mergeCoverageReports } from './merge-coverage-reports';
 import { makeCoverageReports } from './make-coverage-reports';
