04208545618b64a49b55401e5ac24b9618231f3f
Matthew Heroux <matthewheroux@gmail.com>
2023-03-20 09:02:42 -0500

    feat: flesh in html to pdf (#255)
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

---
+++ services/html-to-pdf/bin/app.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { HtmlToPdfStack } from '../stacks/html-to-pdf-stack';
+
+const app = new cdk.App();
+
+new HtmlToPdfStack(app, 'HtmlToPdfStack', {});
