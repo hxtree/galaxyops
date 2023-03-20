22b4996d04b5418e30d527b84836d5868585e7fd
hxtree <matthew.heroux@gmail.com>
2023-03-20 02:35:21 +0000

    feat: health check
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/character-sheet/src/app.module.ts
+++ services/character-sheet/src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { ArchetypeModule } from './module/archetype/archetype.module';
 import { CharacterSheetModule } from './module/character-sheet/character-sheet.module';
 import { GearModule } from './module/gear/gear.module';
+import { HealthModule } from './module/health/health.module';
 import { SkillModule } from './module/skill/skill.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    HealthModule,
     ArchetypeModule,
     GearModule,
     SkillModule,
--- services/luck-by-dice/src/app.module.ts
+++ services/luck-by-dice/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DiceRollModule } from './module/dice-roll/dice-roll.module';
+import { HealthModule } from './module/health/health.module';
 
 @Module({
-  imports: [DiceRollModule],
+  imports: [DiceRollModule, HealthModule],
   providers: [],
   exports: [],
 })
