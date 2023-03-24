7749643ab8b0b14fbdeee9623b2fb0fb0c4d737d
Matthew Heroux <matthewheroux@gmail.com>
2023-03-23 21:44:46 -0500

    feat: html service cleanup (#257)
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/html-to-pdf/src/module/pdf/pdf.controller.ts
+++ services/html-to-pdf/src/module/pdf/pdf.controller.ts
@@ -13,21 +13,8 @@ export class PdfController {
     @Res() res: Response,
     @Body() body: CreateUrlToPdfDto,
   ) {
-    const options = {
-      page: 1,
-      headerTemplate: '<p></p>',
-      footerTemplate: '<p></p>',
-      displayHeaderFooter: false,
-      margin: {
-        top: '50px',
-        bottom: '50px',
-      },
-      printBackground: true,
-      path: 'invoice.pdf',
-    };
-
     const buffer = await this.pdfService.renderUrl(body.url);
-    this.responseAsPdf(false, buffer, res, body.filename || '');
+    this.responseAsPdf(false, buffer, res, body.filename);
   }
 
   @Post('render-html')
@@ -48,19 +35,13 @@ export class PdfController {
     if (!json) {
       const stream = this.pdfService.createReadableStream(buffer);
       res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'attachment; filename=result.pdf');
-      if (filename) {
-        res.setHeader(
-          'Content-Disposition',
-          `attachment; filename=${filename}`,
-        );
-      }
+      res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
       stream.pipe(res);
     } else {
       res.setHeader('Content-Type', 'application/json;charset=UTF-8');
       res.status(200).send({
         content: buffer.toString('base64'),
-        filename: filename || 'result.pdf',
+        filename,
         mimeType: 'application/pdf',
       });
     }
