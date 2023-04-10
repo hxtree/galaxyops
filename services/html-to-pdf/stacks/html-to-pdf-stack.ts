5edfda64a47bfa769202b84517b37192bc393306
Matthew Heroux <matthew.heroux@gmail.com>
2023-04-09 22:11:32 -0500

    fix: build issues (#281)
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/html-to-pdf/stacks/html-to-pdf-stack.ts
+++ services/html-to-pdf/stacks/html-to-pdf-stack.ts
@@ -2,6 +2,7 @@ import { Microservice } from '@cats-cradle/constructs';
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import { StackProps } from 'aws-cdk-lib';
+import * as path from 'path';
 
 export class HtmlToPdfStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,6 +10,7 @@ export class HtmlToPdfStack extends cdk.Stack {
 
     const microservice = new Microservice(this, 'html-to-pdf-stack', {
       path: 'html-to-pdf',
+      projectRoot: path.join(__dirname, '..'),
     });
 
     new cdk.CfnOutput(this, 'Localhost API Example', {
