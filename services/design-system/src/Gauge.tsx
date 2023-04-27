728e05bf0a1f47db09b12ac4081fc4895f77215f
hxtree <matthew.heroux@gmail.com>
2023-04-27 17:46:09 +0000

    feat: add left and right guages
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/design-system/src/Gauge.tsx
+++ services/design-system/src/Gauge.tsx
@@ -1,56 +1,62 @@
 import React from 'react';
 
+export enum OrientationType {
+  RIGHT = 'RIGHT',
+  LEFT = 'LEFT',
+}
+
 export type GaugeProps = {
   color?: string;
-  stroke?: number;
+  strokeWidth?: number;
+  orientation?: OrientationType;
   current?: number;
   max?: number;
-  mirror?: boolean; // determine whether right or left orientation
 };
 
 export const Gauge = (props: GaugeProps) => {
-  const stroke = props.stroke ?? 7;
-  const isRight = true;
-  const isLeft = false; //true;
+  const strokeWidth = props.strokeWidth ?? 7;
+  const color = props.color ?? 'green';
+  const isLeft = props.orientation === OrientationType.LEFT;
+  const isRight = props.orientation !== OrientationType.LEFT;
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-      <path
-        fill="none"
-        stroke="red"
-        stroke-width={stroke}
-        d="M20,0 a20,20 0 0,1 20,20"
-      />
       {isLeft && (
-        <path
-          fill="none"
-          stroke="blue"
-          stroke-width={stroke}
-          d="M40,20 a20,20 0 0,4 -20,20"
-        />
+        <>
+          <path
+            fill="none"
+            stroke={color}
+            stroke-width={strokeWidth}
+            d="M50,30 a20,20 0 1,0 -20,20"
+          />
+          <line
+            x1="30"
+            y1="50"
+            x2="120"
+            y2="50"
+            stroke={color}
+            stroke-width={strokeWidth}
+          />
+        </>
       )}
       {isRight && (
-        <path
-          fill="none"
-          stroke="green"
-          stroke-width={stroke}
-          d="M20,40 a20,20 0 0,1 -20,-20"
-        />
+        <>
+          <path
+            fill="none"
+            stroke={color}
+            stroke-width={strokeWidth}
+            d="M120,50 a20,20 0 1,0 -20,-20"
+          />
+          <line
+            x1="20"
+            y1="50"
+            x2="120"
+            y2="50"
+            stroke={color}
+            stroke-width={strokeWidth}
+          />
+        </>
       )}
-      <path
-        fill="none"
-        stroke="yellow"
-        stroke-width={stroke}
-        d="M0,20 a20,20 0 0,1 20,-20"
-      />
-      <line
-        x1="20"
-        y1="40"
-        x2="100"
-        y2="40"
-        stroke="black"
-        stroke-width={stroke}
-      />
     </svg>
   );
 };
