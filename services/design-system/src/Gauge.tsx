db9bce6269a1586b72762d70cda960f78ec4a440
Matthew Heroux <matthew.heroux@gmail.com>
2023-04-30 21:31:09 -0500

    feat: add meter hud (#323)
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/design-system/src/Gauge.tsx
+++ services/design-system/src/Gauge.tsx
@@ -29,52 +29,37 @@ export const Gauge = (props: GaugeProps) => {
 
   const yStartPoint = radius + strokeWidth / 2;
   const xStartPoint = radius * 2 + strokeWidth / 2;
-  const leftPaths = `M${xStartPoint},${yStartPoint}  a${radius},${radius} 0 1,0 -${radius},${radius}`;
+  const leftPaths = `M${xStartPoint},${yStartPoint}
+    a${radius},${radius} 0 1,0 -${radius},${radius}
+    m 0, 0 h ${width}
+    `;
 
-  const rightXLineStartPoint = 0;
   const rightXLineEndPoint = width - radius;
   const rightXStartPoint = width - radius - strokeWidth / 2;
   const rightYStartPoint = radius * 2 + strokeWidth / 2;
-  const rightPaths = `M${rightXStartPoint},${rightYStartPoint} a${radius},${radius} 0 1,0 -${radius},-${radius}`;
+  const rightPaths = `M${rightXStartPoint},${rightYStartPoint}
+    a${radius},${radius} 0 1,0 -${radius},-${radius}
+    m ${radius}, ${radius} h -${rightXLineEndPoint}
+    `;
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
       {props.boundingBox && <rect width={width} height={height} />}
       {isLeft && (
-        <>
-          <path
-            fill="none"
-            stroke={color}
-            stroke-width={strokeWidth}
-            d={leftPaths}
-          />
-          <line
-            x1={yStartPoint}
-            y1={xStartPoint}
-            x2={width}
-            y2={xStartPoint}
-            stroke={color}
-            stroke-width={strokeWidth}
-          />
-        </>
+        <path
+          fill="pink"
+          stroke={color}
+          stroke-width={strokeWidth}
+          d={leftPaths}
+        />
       )}
       {isRight && (
-        <>
-          <path
-            fill="none"
-            stroke={color}
-            stroke-width={strokeWidth}
-            d={rightPaths}
-          />
-          <line
-            x1={rightXLineStartPoint}
-            y1={xStartPoint}
-            x2={rightXLineEndPoint}
-            y2={xStartPoint}
-            stroke={color}
-            stroke-width={strokeWidth}
-          />
-        </>
+        <path
+          fill="pink"
+          stroke={color}
+          stroke-width={strokeWidth}
+          d={rightPaths}
+        />
       )}
     </svg>
   );
