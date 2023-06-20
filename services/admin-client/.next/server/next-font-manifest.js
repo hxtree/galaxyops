187c6265449856ae47f8466162d18399980e8eae
hxtree <matthew.heroux@gmail.com>
2023-06-20 05:30:45 +0000

    feat: remove source files
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/admin-client/.next/server/next-font-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__NEXT_FONT_MANIFEST =
-  '{"pages":{},"app":{"/usr/src/app/common/temp/node_modules/.pnpm/next@13.4.6_react-dom@18.2.0_react@18.2.0/node_modules/next/font/google/target.css?{\\"path\\":\\"src/app/layout.tsx\\",\\"import\\":\\"Inter\\",\\"arguments\\":[{\\"subsets\\":[\\"latin\\"]}],\\"variableName\\":\\"inter\\"}":["static/media/2aaf0723e720e8b9-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
