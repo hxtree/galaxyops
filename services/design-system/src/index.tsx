62b04ad1ba41b8748b254e797b1464eed39f7910
Matthew Heroux <matthew.heroux@gmail.com>
2023-06-21 23:19:54 -0500

    feat: code snippet hljs (#355)
    ---------
    
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/design-system/src/index.tsx
+++ services/design-system/src/index.tsx
@@ -3,6 +3,7 @@
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
 
+export * from './CodeSnippet/CodeSnippet';
 export * from './Layout';
 export * from './Button';
 export * from './Checkbox';
@@ -12,7 +13,6 @@ export * from './FileUpload';
 export * from './Typography';
 export * from './Gauge/Gauge';
 export * from './Meter';
-export * from './Code/Code';
 export * from './LoadingButton';
 export * from './Link';
 export * from './NonInteractiveLink';
