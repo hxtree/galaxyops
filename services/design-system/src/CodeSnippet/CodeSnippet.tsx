12e529a03256faf148751cf3be01f111c3f30ed2
hxtree <matthew.heroux@gmail.com>
2023-06-27 19:46:46 +0000

    fix: remove dead code
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/design-system/src/CodeSnippet/CodeSnippet.tsx
+++ services/design-system/src/CodeSnippet/CodeSnippet.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-// import highlighterStyle from 'react-syntax-highlighter/dist/esm/styles/hljs/vs2015';
 import { codeTheme } from './hljs-custom';
 
 export enum CodeSnippetLanguages {
