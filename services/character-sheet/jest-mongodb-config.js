98240920cc309e3613410458b68df5ab630731ba
Matthew Heroux <matthew.heroux@gmail.com>
2023-06-26 20:06:06 -0500

    feat: switch to jest mongo in memory server instance (#359)
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

---
+++ services/character-sheet/jest-mongodb-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mongodbMemoryServerOptions: {
+    binary: {
+      version: '4.2.8',
+      skipMD5: true,
+    },
+    instance: {},
+    autoStart: false,
+  },
+  mongoURLEnvName: 'MONGO_DATABASE_URI',
+};
