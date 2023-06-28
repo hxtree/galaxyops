a13d41e499c557c768ac1e1e026af8a45415bcbb
hxtree <matthew.heroux@gmail.com>
2023-06-28 01:25:59 +0000

    feat: change sb bs method
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/design-system/bin/app.ts
+++ services/design-system/bin/app.ts
@@ -3,4 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 import { MainStack } from '../stacks/main-stack';
 
 const app = new cdk.App();
-new MainStack(app, 'MainStack');
+new MainStack(app, 'MainStack', {
+  synthesizer: new cdk.DefaultStackSynthesizer({
+    generateBootstrapVersionRule: false,
+  }),
+});
