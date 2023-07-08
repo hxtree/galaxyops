92a5aa512fc17738eaeb862b42f9392230b1d846
hxtree <matthew.heroux@gmail.com>
2023-07-08 05:04:00 +0000

    feat: add navbar to all pages
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/admin-client/pages/archetypes.tsx
+++ services/admin-client/pages/archetypes.tsx
@@ -9,6 +9,14 @@ export default function Archetype() {
       <div className={styles.description}>
         <Container>
           <h1>Archetypes</h1>
+          <p>
+            Archetypes act as the foundation and recipe for character sheets,
+            shaping their capabilities and limitations. Each character sheet
+            inherits traits from a specific archetype, determining their
+            potential and defining what they can and cannot learn. Archetype
+            information is not stored in the database, but it serves as a
+            crucial guide for player and non-player character creation.
+          </p>
           <ArchetypeSelect />
         </Container>
       </div>
--- services/admin-client/src/app/layout.tsx
+++ services/admin-client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Layout from '../../components/Layout';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
