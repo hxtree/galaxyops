dc431924375d5f6ab38965d7615090efe39ecce7
Matthew Heroux <matthew.heroux@gmail.com>
2023-09-09 19:18:25 -0500

    feat: add character default (#422)
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/character-sheet/src/modules/character-sheet/create-character-sheet-dto.ts
+++ services/character-sheet/src/modules/character-sheet/create-character-sheet-dto.ts
@@ -2,29 +2,38 @@ import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEnum, IsOptional, IsString, IsUUID,
 } from 'class-validator';
+import { v4 } from 'uuid';
 import { ArchetypeId, ArchetypeIds } from '../../data/archetype';
 
 export class CreateCharacterSheetDto {
-  @ApiProperty()
+  @ApiProperty({
+    default: v4(),
+  })
   @IsUUID()
   @IsOptional()
   public _id?: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    default: v4(),
+  })
   @IsUUID()
   public instanceId!: string;
 
-  @ApiProperty({ enum: ArchetypeIds })
+  @ApiProperty({ enum: ArchetypeIds, default: 'DARUMA_NAKAMURA' })
   @IsEnum(ArchetypeIds)
   public archetypeId!: ArchetypeId;
 
   @IsString()
   @IsOptional()
-  @ApiProperty()
+  @ApiProperty({
+    default: 'Daruma',
+  })
   public name?: string;
 
   @IsString()
   @IsOptional()
-  @ApiProperty()
+  @ApiProperty({
+    default: 'Nakamura',
+  })
   public surname?: string;
 }
