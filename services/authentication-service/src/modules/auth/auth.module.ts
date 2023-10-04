565b1ebfc2aef92f8ecaa4523348510f249e83e2
hxtree <matthew.heroux@gmail.com>
2023-10-04 11:29:47 +0000

    feat: jwt progress and pkg updates
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/authentication-service/src/modules/auth/auth.module.ts
+++ services/authentication-service/src/modules/auth/auth.module.ts
@@ -2,18 +2,20 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { CognitoService } from './cognito.service';
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { jwtConstants } from './constants';
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.register({
-      secret: 'YOUR_SECRET_KEY', // TODO set
-      signOptions: { expiresIn: '1h' },
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
     }),
   ],
+  providers: [CognitoService, AuthService, JwtStrategy],
   controllers: [AuthController],
-  providers: [CognitoService, AuthService],
 })
 export class AuthModule {}
