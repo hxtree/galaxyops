f64a5c99635fe49cca2cd63e6ab720cfc4cdbf99
hxtree <matthew.heroux@gmail.com>
2023-10-28 02:59:58 +0000

    feat: rename bundler to create-bundle
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- platform/create-bundle/src/create-bundle-bin.ts
+++ platform/create-bundle/src/create-bundle-bin.ts
@@ -4,6 +4,8 @@ import { NestJsBundler } from './profiles/nestjs-bundler';
 const projectRoot = process.cwd();
 const watchMode = process.argv.includes('--watch');
 
+// TODO add support for other bundle profiles based on flags as necessary
+
 new NestJsBundler({
   projectRoot,
   watch: !!watchMode,
