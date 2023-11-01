62518c72de9178787b4f40f0bb27a79638842ea4
hxtree <matthew.heroux@gmail.com>
2023-11-01 04:57:33 +0000

    chore: remove dead code
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- libraries/faker-factory/src/get-schemas.ts
+++
@@ -1,6 +0,0 @@
-import { validationMetadatasToSchemas } from 'class-validator-jsonschema';
-import { SchemaObject } from 'openapi3-ts';
-
-export function getSchemas(): Record<string, SchemaObject> {
-  return validationMetadatasToSchemas();
-}
