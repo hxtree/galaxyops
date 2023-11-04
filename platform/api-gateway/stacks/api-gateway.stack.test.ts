f91fb1d2b1e1e79016720a87fdeca709dd8e19a1
hxtree <matthew.heroux@gmail.com>
2023-11-04 20:38:12 +0000

    fix: deployment hash dynamic in snapshot
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- platform/api-gateway/stacks/api-gateway.stack.test.ts
+++ platform/api-gateway/stacks/api-gateway.stack.test.ts
@@ -7,6 +7,14 @@ describe('ApiGatewayStack', () => {
     const app = new cdk.App();
     const stack = new ApiGatewayStack(app, 'MyTestStack');
     const template = Template.fromStack(stack);
+
+    // TODO consider reworking to use DNS instead of incrementing and deploying each time
+    const customSnapshotSerializer = {
+      test: (val: any) => typeof val === 'string',
+      print: (val: any) => val.replace(/deployment\d{8}T\d{9}Z[^:]+/, 'deployment_UNIQUE_HASH'),
+    };
+    expect.addSnapshotSerializer(customSnapshotSerializer);
+
     expect(template).toMatchSnapshot();
   });
 });
