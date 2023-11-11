1f5007612597980018085ee42bccbccd7b510794
hxtree <matthew.heroux@gmail.com>
2023-11-11 09:35:14 +0000

    fix: guassian random
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- libraries/pseudo-random/src/gaussian-random.test.ts
+++ libraries/pseudo-random/src/gaussian-random.test.ts
@@ -1,27 +1,16 @@
 import { GaussianRandom } from './gaussian-random';
 
 describe('GaussianRandom', () => {
-  it.each(Array.from({ length: 100 }, (_, index) => index + 1))(
+  it.each(Array.from({ length: 100 }, (_, index) => index * 9997))(
     'Run %i: should generate numbers with a Gaussian distribution',
-    (runNumber) => {
+    (seed) => {
       const gaussianRandom = new GaussianRandom(0, 1);
+      const seedString = seed.toString().padStart(8, '0');
 
-      const result = gaussianRandom.generate();
+      const result = gaussianRandom.generate(seedString);
 
       // Snapshot to ensure consistent results across runs
       expect(result).toMatchSnapshot();
     },
   );
-
-  it('should generate numbers with the specified mean and standard deviation', () => {
-    const mean = 5;
-    const standardDeviation = 2;
-    const gaussianRandom = new GaussianRandom(mean, standardDeviation);
-
-    const result = gaussianRandom.generate();
-
-    // Check if the result is within an acceptable range based on the mean and standard deviation
-    expect(result).toBeGreaterThanOrEqual(mean - 3 * standardDeviation);
-    expect(result).toBeLessThanOrEqual(mean + 3 * standardDeviation);
-  });
 });
