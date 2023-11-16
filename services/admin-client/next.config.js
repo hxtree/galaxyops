4706943edf9e893b2e80ddd197ee6a0843fab96d
hxtree <matthew.heroux@gmail.com>
2023-11-16 01:16:34 +0000

    feat: ssg output
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/admin-client/next.config.js
+++ services/admin-client/next.config.js
@@ -1,2 +1,18 @@
-/** @type {import('next').NextConfig} */
-module.exports = {};
+/**
+ * @type {import('next').NextConfig}
+ * https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+ */
+const nextConfig = {
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
+
+module.exports = nextConfig;
