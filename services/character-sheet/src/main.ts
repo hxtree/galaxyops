aaeb899164a28cb1c4096662089941b9695a3544
hxtree <matthew.heroux@gmail.com>
2023-11-23 05:20:59 +0000

    feat: dynamic description
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/character-sheet/src/main.ts
+++ services/character-sheet/src/main.ts
@@ -2,8 +2,9 @@
 import { writeFileSync } from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
+import packageJson from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,15 +16,22 @@ async function bootstrap() {
   });
 
   const config = new DocumentBuilder()
-    .setTitle('@cats-cradles/character-sheet')
-    .setDescription('An API for the character sheet service')
-    .setVersion('1.0')
+    .setTitle(packageJson.name)
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   // update the openapi-spec
   writeFileSync('./openapi-spec.json', JSON.stringify(document));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   await app.listen(3000);
 }
