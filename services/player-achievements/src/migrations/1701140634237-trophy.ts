fc2dd80f32c3368051a10cf9e37afbd23c551770
hxtree <matthew.heroux@gmail.com>
2023-11-29 00:46:32 +0000

    feat: migrations trophies
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/player-achievements/src/migrations/1701140634237-trophy.ts
+++ services/player-achievements/src/migrations/1701140634237-trophy.ts
@@ -1,12 +1,18 @@
 import { asyncForEach } from '@cats-cradle/nestjs-modules';
+import { ObjectId, UUID } from 'bson';
+import { v4 } from 'uuid';
 import { getDb } from '../migrations-utils/db';
 import { Achievements } from '../models/achievements';
+
+// import { UUID } from 'mongodb';
 
 export const up = async () => {
   const db = await getDb();
 
   await asyncForEach(Achievements, async (trophy) => {
-    await db.collection('trophies').insertOne(trophy);
+    const record = { _id: new UUID(v4()) }; // ObjectId(new UUID()) };
+
+    await db.collection('trophies').insertOne({ ...record, ...trophy });
   });
 };
 
