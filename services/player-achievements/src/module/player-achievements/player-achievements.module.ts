9a5ecf14fab993d54b1b09fd6ed5b03bc9d37f47
hxtree <matthew.heroux@gmail.com>
2023-11-30 03:18:13 +0000

    feat: add achievements to module
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/player-achievements/src/module/player-achievements/player-achievements.module.ts
+++ services/player-achievements/src/module/player-achievements/player-achievements.module.ts
@@ -4,15 +4,18 @@ import { PlayerAchievementController } from './player-achievements.controller';
 import { PlayerAchievementRepository } from '../../models/player-achievement.repository';
 import { databaseModule } from '../../database.module';
 import { PlayerAchievementSchema } from '../../models/player-achievement.schema';
+import { AchievementSchema } from '../../models/achievement.schema';
+import { AchievementRepository } from '../../models/achievement.repository';
 
 @Module({
   imports: [
     databaseModule(),
     MongooseModule.forFeature([
       { name: 'PlayerAchievement', schema: PlayerAchievementSchema },
+      { name: 'Achievement', schema: AchievementSchema },
     ]),
   ],
   controllers: [PlayerAchievementController],
-  providers: [PlayerAchievementRepository],
+  providers: [PlayerAchievementRepository, AchievementRepository],
 })
 export class PlayerAchievementModule {}
