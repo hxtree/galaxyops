52d5c5f28dbd726024fb6f20a64115e8027caa06
hxtree <matthew.heroux@gmail.com>
2023-12-07 01:54:53 +0000

    feat: add footer to design
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

---
+++ services/design-system/stories/components/organisms/PageFooter.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { PageFooter, PageFooterProps } from '../../../src';
+
+export default {
+  title: 'Organisms/PageFooter',
+  component: PageFooter,
+} as Meta<typeof PageFooter>;
+
+export const Default = (args: PageFooterProps) => <PageFooter {...args}></PageFooter>;
+
+Default.args = {
+  siteOwner: 'Cat\'s Cradle',
+};
