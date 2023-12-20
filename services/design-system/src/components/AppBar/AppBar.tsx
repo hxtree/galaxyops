18b74ccf35bf2a07b002f13dc7be2067abf11c72
hxtree <matthew.heroux@gmail.com>
2023-12-20 14:13:29 +0000

    feat: fix app bar with react router
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/design-system/src/components/AppBar/AppBar.tsx
+++ services/design-system/src/components/AppBar/AppBar.tsx
@@ -1,4 +1,5 @@
-import './style.module.scss';
+import './style.scss';
+import React from "react";
 
 export type NavMenuItem = {
   link: string;
@@ -9,36 +10,30 @@ export type AppBarProps = {
   menuItems?: NavMenuItem[];
   siteTitle?: string;
   theme?: 'dark' | 'light';
+  children?: any;
 }
 
 export const AppBar = (props: AppBarProps) => {
-  const { siteTitle, menuItems, theme } = props;
+  const { children, siteTitle, menuItems, theme } = props;
 
   return (
     <>
-    <nav className={`navbar navbar-expand-lg navbar-light`}>
-      <div className="container">
-            <a className="navbar-brand" href="#">{siteTitle || 'Your Brand'}</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+      <nav className={`navbar navbar-expand-lg navbar-light`}>
+        <div className="container">
+          <a className="navbar-brand" href="#">{siteTitle || 'Your Brand'}</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
-    </nav>
+      </nav>
 
-    <nav className="navbar navbar-expand-lg navbar-dark app-bar-dark">
-    <div className="container">
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav me-auto">
-          {menuItems && menuItems.map((menuItem: NavMenuItem) => (
-          <li className="nav-item">
-            <a className="nav-link" href={menuItem.link}>{menuItem.title}</a>
-          </li>
-          ))
-          }
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark app-bar-dark">
+        <div className="container">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            {children}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </>
   );
 };
