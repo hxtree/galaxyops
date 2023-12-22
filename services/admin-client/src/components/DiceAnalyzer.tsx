d91f6f3d561c76a3cc0d7fbacdf902d400e262c9
Matthew Heroux <matthew.heroux@gmail.com>
2023-12-21 23:39:03 -0600

    Feature/add disable button (#767)
    ---------
    
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/admin-client/src/components/DiceAnalyzer.tsx
+++ services/admin-client/src/components/DiceAnalyzer.tsx
@@ -122,22 +122,21 @@ export const DiceAnalyzer = (props: DiceAnalyzerProps) => {
           </Stack>
         </Grid>
         <div>
-        <Button
-      color="secondary"
-      onClick={() => clear()}
-      // TODO clear should be disabled when already cleared
-      // disabled="true"
-      testId={`dice-analyzer-clear`}
-      ref={(ref: any) => analytics.set(ref, 'Clear')}>
-      Clear
+      <Button
+        color="primary"
+        loading={isLoading}
+        onClick={() => callApi()}
+        testId={`dice-analyzer-roll`}
+        ref={(ref: any) => analytics.set(ref, 'Roll')}>
+        Roll
       </Button>
       <Button
-      color="primary"
-      loading={isLoading}
-      onClick={() => callApi()}
-      testId={`dice-analyzer-roll`}
-      ref={(ref: any) => analytics.set(ref, 'Roll')}>
-      Roll
+        color="secondary"
+        onClick={() => clear()}
+        disabled={data.length < 1}
+        testId={`dice-analyzer-clear`}
+        ref={(ref: any) => analytics.set(ref, 'Clear')}>
+        Clear
       </Button>
 
 
