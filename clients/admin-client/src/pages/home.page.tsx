f6c50bcf9443b82121e71929c6117f3957117cbb
hxtree <matthew.heroux@gmail.com>
2023-12-24 01:31:35 +0000

    feat: add character sheet page
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- clients/admin-client/src/pages/home.page.tsx
+++ clients/admin-client/src/pages/home.page.tsx
@@ -1,15 +1,7 @@
-import { Container } from '@cats-cradle/design-system/dist/main';
-
 export default function HomePage() {
   return (
-    <>
-     <main>
-       <div>
-        <Container>
-          <h1>Game Master Portal</h1>
-        </Container>
-       </div>
+    <main className="container">
+      <h1>Game Master Portal</h1>
     </main>
-    </>
   );
 }
