f787227130e303fb3e04a89485df7663963fa5fb
Matthew Heroux <matthew.heroux@gmail.com>
2024-01-10 00:14:20 -0600

    feat: add meta tags endpoint to html to pdf (#789)
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/html-to-pdf/src/app.module.ts
+++ services/html-to-pdf/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './module/health/health.module';
 import { PdfModule } from './module/pdf/pdf.module';
+import { MetaTagsModule } from './module/meta-tags/meta-tags.module';
 
 @Module({
-  imports: [HealthModule, PdfModule],
+  imports: [HealthModule, PdfModule, MetaTagsModule],
   providers: [],
   exports: [],
 })
