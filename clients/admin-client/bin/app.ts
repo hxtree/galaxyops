07b4cc72f3cfa1178a2c681ead14af3109c8375f
hxtree <matthew.heroux@gmail.com>
2024-02-04 05:22:51 +0000

    feat: add admin client deployment
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- clients/admin-client/bin/app.ts
+++ clients/admin-client/bin/app.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-// TODO deploy to AWS
-// import * as cdk from 'aws-cdk-lib';
-// import { AdminClientStack } from '../stacks/admin-client-stack';
+import * as cdk from 'aws-cdk-lib';
+import { AdminClientStack } from '../stacks/admin-client.stack';
 
-// const app = new cdk.App();
+const app = new cdk.App();
 
-// new AdminClientStack(app, 'AdminClientStack', {});
+new AdminClientStack(app, 'AdminClientStack', {});
--- clients/admin-client/stacks/admin-client.stack.ts
+++ clients/admin-client/stacks/admin-client.stack.ts
@@ -1,49 +1,121 @@
-// import * as cdk from 'aws-cdk-lib';
-// import * as s3 from 'aws-cdk-lib/aws-s3';
-// import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-// import * as route53 from 'aws-cdk-lib/aws-route53';
-// import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import { StackProps } from 'aws-cdk-lib';
+import { Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import {
+  Distribution,
+  OriginAccessIdentity,
+  ResponseHeadersPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 
-// const app = new cdk.App();
-// const stack = new cdk.Stack(app, 'CloudFrontStack');
+export class AdminClientStack extends cdk.Stack {
+  public parentDomainName: string;
+  public acmCertificateArn: string;
+  public hostedZoneId: string;
 
-// // Define your S3 bucket for hosting the contents of the 'src' directory
-// const srcBucket = new s3.Bucket(stack, 'SrcBucket', {
-//   removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for demonstration purposes
-// });
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-// // Upload the contents of the 'src' directory to the S3 bucket
-// srcBucket.addDirectory('./src', {
-//   destinationKeyPrefix: 'src', // This will create 'src' prefix in the S3 bucket
-// });
+    // s3 bucket
+    const bucket = new Bucket(this, 'Bucket', {
+      accessControl: BucketAccessControl.PRIVATE,
+    });
 
-// // Define your CloudFront distribution
-// const cloudFrontDistribution = new cloudfront.CloudFrontWebDistribution(stack, 'MyCloudFrontDistribution', {
-//   originConfigs: [
-//     {
-//       s3OriginSource: {
-//         s3BucketSource: srcBucket,
-//       },
-//       behaviors: [{ isDefaultBehavior: true }],
-//     },
-//   ],
-// });
+    const originAccessIdentity = new OriginAccessIdentity(
+      this,
+      'OriginAccessIdentity',
+    );
+    bucket.grantRead(originAccessIdentity);
 
-// // Define your custom domain
-// const domainName = 'example.com';
+    this.acmCertificateArn = ssm.StringParameter.fromStringParameterAttributes(
+      this,
+      `${id}-orgformation-certs-wildcard-cert1-arn`,
+      {
+        parameterName: 'orgformation-certs-wildcard-cert1-arn',
+      },
+    ).stringValue;
 
-// // Create a Route 53 hosted zone (replace hostedZoneId with your actual Hosted Zone ID)
-// const hostedZoneId = 'your-hosted-zone-id';
-// const hostedZone = route53.HostedZone.fromHostedZoneAttributes(stack, 'HostedZone', {
-//   hostedZoneId: hostedZoneId,
-//   zoneName: domainName,
-// });
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      `${id}-acm-certificate`,
+      this.acmCertificateArn,
+    );
 
-// // Create a record set for the custom domain pointing to the CloudFront distribution
-// new route53.ARecord(stack, 'AliasRecord', {
-//   recordName: domainName,
-//   target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cloudFrontDistribution)),
-//   zone: hostedZone,
-// });
+    // DNS
 
-// app.synth();
+    // fetch parameters from SSM Parameter Store
+    this.parentDomainName = ssm.StringParameter.fromStringParameterAttributes(
+      this,
+      `${id}-ssm-domain-name`,
+      {
+        parameterName: 'DOMAIN_NAME',
+      },
+    ).stringValue;
+
+    // Retrieve parameters from SSM Parameter Store
+    this.hostedZoneId = ssm.StringParameter.fromStringParameterAttributes(
+      this,
+      `${id}-hosted-zone-id`,
+      {
+        parameterName: 'my-domains-hosted-zone-id',
+      },
+    ).stringValue;
+
+    const subdomainName = 'admin';
+
+    const domainName = `${subdomainName}.${this.parentDomainName}`;
+
+    // cloudfront distribution
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const cloudFrontDistribution = new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(bucket, { originAccessIdentity }),
+        // todo lock down CORS later
+        responseHeadersPolicy:
+          ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS_WITH_PREFLIGHT,
+      },
+      domainNames: [domainName],
+      certificate,
+    });
+
+    // bucket resource
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const awsBucketResource = new BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: bucket,
+      sources: [Source.asset('./dist')],
+    });
+
+    const hostedZone = route53.PublicHostedZone.fromHostedZoneAttributes(
+      this,
+      `${id}-hosted-zone`,
+      {
+        hostedZoneId: this.hostedZoneId,
+        zoneName: domainName,
+      },
+    );
+
+    // Create a record set for the custom domain pointing to the CloudFront distribution
+    new route53.ARecord(this, 'AliasRecord', {
+      recordName: domainName,
+      target: route53.RecordTarget.fromAlias(
+        new targets.CloudFrontTarget(cloudFrontDistribution),
+      ),
+      zone: hostedZone,
+    });
+
+    new cdk.CfnOutput(this, 'Cloud Front Distribution', {
+      value: cloudFrontDistribution.domainName,
+    });
+
+    new cdk.CfnOutput(this, 'Domain Name', {
+      value: `https://${domainName}`,
+    });
+  }
+}
