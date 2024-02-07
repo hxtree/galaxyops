642cb3854fd502b9102df03f25b20ad9bee5dbb2
hxtree <matthew.heroux@gmail.com>
2024-02-07 04:58:40 +0000

    feat: add footer links
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- clients/admin-client/src/App.tsx
+++ clients/admin-client/src/App.tsx
@@ -3,6 +3,8 @@ import { PageFooter, faGithub } from '@cats-cradle/design-system/dist/main';
 import { Router } from './routing/Router';
 
 function App() {
+  const parentDomainName = import.meta.env.VITE_PARENT_DOMAIN_NAME ?? 'sandbox.nekosgate.com';
+
   return (
     <>
       <Router/>
@@ -10,9 +12,12 @@ function App() {
         socialMedias={[
           {icon: faGithub, label: 'Github', url: 'https://github.com/hxtree/cats-cradle'},
         ]}
-        links={[]}
         siteOwner="Cats Cradle"
-      />
+        links={[
+          {url: `https://jukebox.${parentDomainName}`, label: 'Jukebox'},
+          {url: `https://design.${parentDomainName}`, label: 'Design System'},
+          {url: `https://api.${parentDomainName}`, label: 'Developer API'},
+      ]}/>
     </>
   );
 }
