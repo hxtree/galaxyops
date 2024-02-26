ebcc9a08491f8d0cc1fa33fcfff6af2281e9d938
hxtree <matthew.heroux@gmail.com>
2024-02-26 13:45:06 +0000

    feat: flesh in topic front
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- clients/design-system/src/components/Hero/Hero.tsx
+++ clients/design-system/src/components/Hero/Hero.tsx
@@ -7,22 +7,43 @@ export interface HeroProps {
   lead: string;
   image: string;
   children?: React.ReactNode
+  pageTier?: number;
 }
 
-export const Hero = ({ image, heading, lead, children }: HeroProps) => {
-  return (
-    <>
-      <div className='hero container' style={{backgroundImage: `url(${image})`}}>
-        <div className='container p-sm-1 p-md-3 p-lg-5'>
-          <div className="row">
-            <div className='col-lg-7 col-sm-12'>
-              <h1 className='display-3 text-primary' dangerouslySetInnerHTML={{ __html: xss(heading) }}></h1>
-              <p className="lead text-white" dangerouslySetInnerHTML={{ __html: xss(lead) }}></p>
-              {children}
+export const Hero = (props: HeroProps) => {
+  const { image, heading, lead, children, pageTier } = props;
+
+  if(pageTier === 1 || pageTier === undefined){
+    return (
+      <>
+        <div className='hero container' style={{backgroundImage: `url(${image})`}}>
+          <div className='container p-sm-1 p-md-3 p-lg-5'>
+            <div className="row">
+              <div className='col-lg-7 col-sm-12'>
+                <h1 className='display-3 text-primary' dangerouslySetInnerHTML={{ __html: xss(heading) }}></h1>
+                <p className="lead text-white" dangerouslySetInnerHTML={{ __html: xss(lead) }}></p>
+                {children}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-   </>
-  )
+     </>
+    )
+  }
+
+  return (
+    <>
+        <div className='hero container-fluid' style={{backgroundImage: `url(${image})`}}>
+          <div className='container p-sm-1 p-md-3 p-lg-5'>
+            <div className="row">
+              <div className='col-lg-7 col-sm-12'>
+                <h1 className='display-3 text-primary' dangerouslySetInnerHTML={{ __html: xss(heading) }}></h1>
+                <p className="lead text-white" dangerouslySetInnerHTML={{ __html: xss(lead) }}></p>
+                {children}
+              </div>
+            </div>
+          </div>
+        </div>
+     </>
+    );
 }
