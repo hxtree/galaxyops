4c0b8737c172a190282d084da66aedd64f631bb3
hxtree <matthew.heroux@gmail.com>
2024-06-15 23:46:40 +0000

    feat: aoe maps
    Signed-off-by: hxtree <matthew.heroux@gmail.com>

--- services/character-sheet/src/data/target.effect.ts
+++ services/character-sheet/src/data/target.effect.ts
@@ -1,5 +1,9 @@
+/** When a Player uses an action that action is relative to an AOE map to
+ * Target of a defined type
+ */
 export enum Target {
-  CASTER = 'CASTER',
+  CASTER = 'CASTER', // player
   OPPONENT = 'OPPONENT',
-  AOE = 'AOE', // MULTIPLE
+  ALLY = 'ALLY',
+  AOE = 'AOE', // MULTIPLE, TODO remove?
 }
