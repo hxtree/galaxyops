be82555f28ec000cc91efcf674ed27f99cbb5eb2
hxtree <matthew.heroux@gmail.com>
2024-07-14 00:33:04 -0500

    fix: action menu types

--- clients/design-system/src/components/ActionMenu/ActionMenu.tsx
+++ clients/design-system/src/components/ActionMenu/ActionMenu.tsx
@@ -26,6 +26,11 @@ export type MenuTreeNode = {
 };
 export type MenuTree = MenuTreeNode[];
 
+export type DisplayItem = {
+  name: string;
+  level?: string;
+};
+
 // TODO
 // In character sheet skills should have a parent child relationship instead of a flat array
 
@@ -33,7 +38,7 @@ export const ActionMenu = (props: ActionMenuProps) => {
   const { spacing, testId } = props;
   const [pointers, setPointers] = useState<number[]>([0]);
   const [menuTree, setMenuTree] = useState<MenuTree>([]);
-  const [menuFlat, setMenuFlat] = useState<string[]>([]);
+  const [menuFlat, setMenuFlat] = useState<DisplayItem[]>([]);
 
   useEffect(() => {
     const newSkills: any = [];
@@ -81,32 +86,103 @@ export const ActionMenu = (props: ActionMenuProps) => {
   }, []);
 
   useEffect(() => {
-    const displayItems: string[] = [];
+    function getElementByPointer(
+      array: MenuTree,
+      pointer: number[],
+    ): MenuTreeNode[] {
+      if (pointer.length === 0) {
+        return array;
+      }
+
+      const index = pointer[0];
+      if (index < 0 || index >= array.length) {
+        throw new Error('Index out of bounds');
+      }
+
+      return getElementByPointer(
+        array[index] as unknown as MenuTreeNode[],
+        pointer.slice(1),
+      );
+    }
+
+    const displayItems: DisplayItem[] = [];
     if (pointers.length === 1) {
       menuTree.forEach((menuItem: MenuTreeNode) => {
-        displayItems.push(menuItem.name);
+        displayItems.push({
+          name: menuItem.name,
+          level: menuItem.action?.level,
+        });
       });
       setMenuFlat(displayItems);
       return;
     }
 
-    const menuItems: MenuTreeNode = getElementByPointer(
+    const ancestorPointers = pointers.slice(0, -1);
+    const menuItems = getElementByPointer(
       menuTree,
-      pointers.slice(0, -1),
-    );
-    if (!menuItems || !menuItems.children) {
-      // TODO: action item was selected resetting pointer
-      setPointers(pointers.slice(0, -1));
+      ancestorPointers,
+    ) as unknown as MenuTreeNode;
+
+    if (menuItems?.children === undefined) {
+      setPointers(ancestorPointers);
       return;
     }
 
     menuItems.children.forEach((menuItem: MenuTreeNode) => {
-      displayItems.push(`${menuItem.name} ${menuItem.action?.level}`);
+      displayItems.push({ name: menuItem.name, level: menuItem.action?.level });
     });
+
     setMenuFlat(displayItems);
-  }, [pointers]);
+  }, [pointers, menuTree]);
 
   useEffect(() => {
+    const movePointers = (direction: string) => {
+      if (direction === 'up') {
+        const lastItem = pointers[pointers.length - 1];
+        if (lastItem === 0) {
+          return;
+        }
+        setPointers([...pointers.slice(0, -1), lastItem - 1]);
+      } else if (direction === 'down') {
+        if (pointers.length === 0) {
+          setPointers([1]);
+          return;
+        }
+        const lastItem = pointers[pointers.length - 1];
+        if (lastItem === menuFlat.length - 1) {
+          return;
+        }
+        setPointers([...pointers.slice(0, -1), lastItem + 1]);
+      } else if (direction === 'back') {
+        if (pointers.length === 1) {
+          return;
+        }
+        setPointers([...pointers.slice(0, -1)]);
+        return;
+      } else if (direction === 'select') {
+        setPointers([...pointers, 0]);
+      }
+    };
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      event.preventDefault();
+      switch (event.key) {
+        case 'w':
+          movePointers('up');
+          break;
+        case 's':
+          movePointers('down');
+          break;
+        case 'a':
+          movePointers('back');
+          break;
+        case 'd':
+          movePointers('select');
+          break;
+        default:
+          break;
+      }
+    };
     const handleWindowKeyUp = (event: KeyboardEvent) => handleKeyUp(event);
 
     document.addEventListener('keyup', handleWindowKeyUp);
@@ -115,72 +191,11 @@ export const ActionMenu = (props: ActionMenuProps) => {
     };
   }, [pointers, menuTree, menuFlat]);
 
-  function getElementByPointer(
-    array: MenuTreeNode[],
-    pointer: number[],
-  ): MenuTreeNode[] | MenuTreeNode {
-    if (pointer.length === 0) {
-      return array; // Base case: return the array itself if pointer is empty
+  const levelFormat = (level: string | undefined) => {
+    if (!level) {
+      return '';
     }
-
-    const index = pointer[0];
-    const rest = pointer.slice(1);
-
-    // Check if the index is within bounds
-    if (index < 0 || index >= array.length) {
-      throw new Error('Index out of bounds');
-    }
-
-    // Recursively access the nested array
-    return getElementByPointer(array[index], rest);
-  }
-
-  const movePointers = (direction: string) => {
-    if (direction === 'up') {
-      const lastItem = pointers[pointers.length - 1];
-      if (lastItem === 0) {
-        return;
-      }
-      setPointers([...pointers.slice(0, -1), lastItem - 1]);
-    } else if (direction === 'down') {
-      if (pointers.length === 0) {
-        setPointers([1]);
-        return;
-      }
-      const lastItem = pointers[pointers.length - 1];
-      if (lastItem === menuFlat.length - 1) {
-        return;
-      }
-      setPointers([...pointers.slice(0, -1), lastItem + 1]);
-    } else if (direction === 'back') {
-      if (pointers.length === 1) {
-        return;
-      }
-      setPointers([...pointers.slice(0, -1)]);
-      return;
-    } else if (direction === 'select') {
-      setPointers([...pointers, 0]);
-    }
-  };
-
-  const handleKeyUp = (event: KeyboardEvent) => {
-    event.preventDefault();
-    switch (event.key) {
-      case 'w':
-        movePointers('up');
-        break;
-      case 's':
-        movePointers('down');
-        break;
-      case 'a':
-        movePointers('back');
-        break;
-      case 'd':
-        movePointers('select');
-        break;
-      default:
-        break;
-    }
+    return level.replace('LV', 'Lv.');
   };
 
   return (
@@ -190,20 +205,27 @@ export const ActionMenu = (props: ActionMenuProps) => {
         data-testid={testId ? `${testId}-root` : null}
       >
         <div className={`action-menu-outer-border`}>
-          <div className={`action-menu-inner-border`}>Open Chest</div>
+          <div className={`action-menu-inner-border`}>
+            <ul className={`menu-single`}>
+              <li className={`link`}>Open Chest</li>
+            </ul>
+          </div>
         </div>
 
         <div className={`action-menu-outer-border mt--3`}>
           <div className={`action-menu-inner-border`}>
-            <ul>
-              {menuFlat.map((menuItem: string, index: number) => {
+            <ul className={`menu-multiple`}>
+              {menuFlat.map((menuItem: DisplayItem, index: number) => {
                 const isActive = index == pointers[pointers.length - 1];
                 return (
                   <li
                     key={index}
                     className={`link ${isActive && 'link-active'}`}
                   >
-                    {menuItem}
+                    {menuItem.name}
+                    <span className={`level`}>
+                      {levelFormat(menuItem.level)}
+                    </span>
                   </li>
                 );
               })}
