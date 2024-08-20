51356c7ddb2af9586654604e9b7baf85313b3fc3
hxtree <matthew.heroux@gmail.com>
2024-08-19 23:11:49 -0500

    feat: add y axis support for tile

--- clients/design-system/src/game-assets/Tile/Tile.tsx
+++ clients/design-system/src/game-assets/Tile/Tile.tsx
@@ -4,23 +4,32 @@ import { TileProps } from './TileProps';
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations
 const TILE_WIDTH = 46;
 const TILE_HEIGHT = 36;
+const TILE_X_OFFSET = 22;
+const TILE_Y_OFFSET = 11;
+const POSITION_X_OFFSET = 500;
+const POSITION_Y_OFFSET = 0;
 
 export const Tile: React.FC<TileProps> = ({ tileset, position, tileData }) => {
   // Calculate the source position based on tileIndex
   const sourceX = TILE_WIDTH * tileData.x - TILE_WIDTH;
   const sourceY = TILE_HEIGHT * tileData.y - TILE_HEIGHT;
-  const positionX = (TILE_WIDTH / 2) * (position.x - 1);
-  const positionY = (TILE_HEIGHT / 2) * (position.y - 1);
+  const positionX =
+    TILE_X_OFFSET * position.x +
+    POSITION_X_OFFSET -
+    (TILE_X_OFFSET * position.y - 1);
+  const positionY =
+    TILE_Y_OFFSET * position.x +
+    POSITION_Y_OFFSET +
+    (TILE_Y_OFFSET * position.y - 1);
 
   const style: React.CSSProperties = {
     width: `${TILE_WIDTH}px`,
     height: `${TILE_HEIGHT}px`,
     backgroundImage: `url(${tileset})`,
-    // border: '1px solid black',
     backgroundPosition: `-${sourceX}px -${sourceY}px`,
     position: 'absolute',
-    left: `${positionY}px`,
-    top: `${positionX}px`,
+    left: `${positionX}px`,
+    top: `${positionY}px`,
   };
 
   return <div style={style} />;
--- clients/design-system/src/game-assets/TileMap/TileMap.tsx
+++ clients/design-system/src/game-assets/TileMap/TileMap.tsx
@@ -6,23 +6,44 @@ export const TileMap = (props: TileMapProps) => {
     <div>
       <Tile
         tileset={props.tileset}
-        tileData={{ x: 2, y: 3 }}
+        tileData={{ x: 1, y: 1 }}
         position={{ x: 1, y: 1, z: 0 }}
       />
       <Tile
         tileset={props.tileset}
-        tileData={{ x: 1, y: 3 }}
+        tileData={{ x: 1, y: 1 }}
+        position={{ x: 2, y: 1, z: 0 }}
+      />
+      <Tile
+        tileset={props.tileset}
+        tileData={{ x: 1, y: 1 }}
+        position={{ x: 3, y: 1, z: 0 }}
+      />
+      <Tile
+        tileset={props.tileset}
+        tileData={{ x: 1, y: 1 }}
+        position={{ x: 4, y: 1, z: 0 }}
+      />
+
+      <Tile
+        tileset={props.tileset}
+        tileData={{ x: 1, y: 2 }}
+        position={{ x: 1, y: 2, z: 0 }}
+      />
+      <Tile
+        tileset={props.tileset}
+        tileData={{ x: 1, y: 2 }}
         position={{ x: 2, y: 2, z: 0 }}
       />
       <Tile
         tileset={props.tileset}
-        tileData={{ x: 1, y: 3 }}
-        position={{ x: 3, y: 3, z: 0 }}
+        tileData={{ x: 1, y: 2 }}
+        position={{ x: 3, y: 2, z: 0 }}
       />
       <Tile
         tileset={props.tileset}
-        tileData={{ x: 2, y: 3 }}
-        position={{ x: 2, y: 1, z: 0 }}
+        tileData={{ x: 1, y: 2 }}
+        position={{ x: 4, y: 2, z: 0 }}
       />
     </div>
   );
