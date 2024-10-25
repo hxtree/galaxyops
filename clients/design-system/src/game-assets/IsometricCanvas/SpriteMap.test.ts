51e0ea4c4bb7dc37239057d1da4294326e7f4831
hxtree <19890291+hxtree@users.noreply.github.com>
2024-10-24 20:08:37 -0500

    fix: unit tests for tag detection

--- clients/design-system/src/game-assets/IsometricCanvas/SpriteMap.test.ts
+++ clients/design-system/src/game-assets/IsometricCanvas/SpriteMap.test.ts
@@ -9,8 +9,8 @@ describe('SpriteMap', () => {
 
   describe('parseTags()', () => {
     it.each([
-      { filename: 'castle-wall-4x4.png', tags: ['wall'] },
-      { filename: 'cave-wall-8x4.png', tags: ['wall'] },
+      { filename: 'castle-wall-4x4.png', tags: ['wall', 'debug-mark'] },
+      { filename: 'cave-wall-8x4.png', tags: ['wall', 'debug-mark'] },
       { filename: 'barrel', tags: [] },
     ])('should parse %s filename and return tags %o', ({ filename, tags }) => {
       const sut = spriteMap.parseTags(filename);
