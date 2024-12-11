4c3998369a4cd06b895bce53cb34b577bfa4bbe0
hxtree <19890291+hxtree@users.noreply.github.com>
2024-12-11 00:28:37 -0600

    feat: flesh in game engine

---
+++ clients/player-client/src/core/GameEngine/GameEngine.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export type GameEngineProps = {
+  data: any;
+  updateData: (newData: any) => void;
+};
+
+export const GameEngine: React.FC<GameEngineProps> = () => {
+  // if input up then try to move up
+
+  return null;
+};
--- clients/player-client/src/pages/home.page.tsx
+++ clients/player-client/src/pages/home.page.tsx
@@ -1,12 +1,23 @@
+import React, { useState } from 'react';
 import { IsometricCanvas, IsometricCanvasProps } from '../core/IsometricCanvas';
-import gameState from './game-state.json';
+import gameState from './data/Sewer.json';
 import { InputProvider } from '../context/Input/InputProvider';
 import { Keyboard } from '../core/Keyboard/Keyboard';
+import { GameEngine } from '../core/GameEngine/GameEngine';
 
 export default function HomePage() {
+  const [gameData, setGameData] = useState({ input: '', playerPosition: 0 });
+
+  const updateGameData = (newData: any) => {
+    setGameData(newData);
+  };
+
   return (
     <InputProvider>
       <Keyboard />
+      <GameEngine data={gameData} updateData={updateGameData} />
+
+      {/** TODO use gameData={gameData} */}
       {/* @ts-expect-error needed for types temp */}
       <IsometricCanvas {...(gameState as IsometricCanvasProps)} />
     </InputProvider>
