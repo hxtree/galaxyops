0dba68b7c36c26348d271d14c8fed2ccce8bd341
hxtree <19890291+hxtree@users.noreply.github.com>
2025-12-19 13:32:25 -0600

    feat: set middlewhare stack env

--- middleware/api/bin/app.ts
+++ middleware/api/bin/app.ts
@@ -3,4 +3,10 @@ import * as cdk from 'aws-cdk-lib';
 import { MainStack } from '../stacks/main.stack';
 
 const app = new cdk.App();
-new MainStack(app, 'ApiGatewayStack');
+new MainStack(app, 'ApiGatewayStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT || process.env.AWS_ACCOUNT_ID,
+    region:
+      process.env.CDK_DEFAULT_REGION || process.env.AWS_REGION || 'us-east-1',
+  },
+});
